b4ced8fabaa02f8529a408b63637200e37ec72b6
Shubham Bhatt <shubham@crackerdigital.com>
2024-05-28 22:43:29 +0530

    completed backend

---
+++ dist/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { errorMiddleware } from "./middlewares/error.js";
+import { connectDB } from "./utils/features.js";
+import NodeCache from "node-cache";
+import { config } from "dotenv";
+import morgan from "morgan";
+import Stripe from "stripe";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
+// Importing Routes
+import userRoute from "./routes/user.js";
+import productRoute from "./routes/products.js";
+import orderRoute from "./routes/order.js";
+import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
+// Get the current file and directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Load environment variables from .env file
+config({ path: path.resolve(__dirname, '../.env') });
+const dbPassword = process.env.MONGO_PASSWORD;
+const dbUsername = process.env.MONGO_USERNAME;
+const uri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.oxgyoja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+connectDB(uri);
+const port = process.env.PORT || 4000;
+const stripeKey = process.env.STRIPE_KEY || "";
+// connectDB(mongoURI);
+export const stripe = new Stripe(stripeKey);
+export const myCache = new NodeCache();
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send("API Working with /api/v1");
+});
+// Using Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/payment", paymentRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
+app.use("/uploads", express.static("uploads"));
+app.use(errorMiddleware);
+app.listen(port, () => {
+    console.log(`Express is working on http://localhost:${port}`);
+});
